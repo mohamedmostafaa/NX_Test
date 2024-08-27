b0fb5411feebbe90ad42e08faf2b0b2aab347218
MohamedMostafa <eng.mmostafa_apm@yahoo.com>
2024-08-27 23:28:00 +0300

    main TS

--- apps/api/main.ts
+++ apps/api/main.ts
@@ -3,20 +3,24 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
+import { Logger, VersioningType } from '@nestjs/common';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: NestApplication = await NestFactory.create(AppModule, {
+    abortOnError: false,
+  });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
+  Logger.log(`==========================================================`);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
+  Logger.log(`==========================================================`);
 }
 
 bootstrap();
